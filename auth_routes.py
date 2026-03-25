2a7face2710b568ad64d614a7bff3df22a98be2b
Lucas Guedes <code.guedes@gmail.com>
2026-03-25 09:35:30 -0300

    criar autorizacao oauth2

--- auth_routes.py
+++ auth_routes.py
@@ -1,32 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario, db
-from dependencies import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from main import bcrypt_context, ALGORITHM, SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
+from typing import Optional
+from fastapi.security import OAuth2PasswordRequestForm
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 def criar_token(id_usuario: int, duracao_token: int = ACCESS_TOKEN_EXPIRE_MINUTES):
     data_expiracao = datetime.now(timezone.utc) + timedelta(minutes=int(duracao_token))
-    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    dic_info = {"sub": str(id_usuario), "exp": data_expiracao}
     jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
     return jwt_codificado
 
-def verificar_token(token: str, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
 
-
-def autenticar_usuario(email, senha, session: Session):
+def autenticar_usuario(email: str, senha: str, session: Session) -> Optional[Usuario]:
     usuario = session.query(Usuario).filter(Usuario.email == email).first()
     if not usuario:
-        return False
-    elif not bcrypt_context.verify(senha, usuario.senha):
-        return False
+        return None
+    if not bcrypt_context.verify(senha, usuario.senha):
+        return None
     return usuario
-    
 
 
 @auth_router.post("/")
@@ -34,8 +32,6 @@ async def home():
     """
     Essa é a rota de autenticação, onde você pode fazer login no sistema.
     """
-    
-    
     return {"message": "Você está na rota de autenticação!", "autenticado": False}
 
 @auth_router.post("/criar_conta")
@@ -51,25 +47,36 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
         return {"message": f"Conta criada com sucesso {usuario_schema.nome}!"}
     
     
-@auth_router.post("/login")
+@auth_router.post( "/login" )
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
     usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+        raise HTTPException(status_code=404, detail="Usuário não encontrado ou credenciais inválidas")
     else:
         access_token = criar_token(usuario.id)
         refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return {
             "access_token": access_token,
             "refresh_token": refresh_token,
+            "token_type": "Bearer"
+        }
+
+@auth_router.post("/login-form")
+async def login_form(dados_formulario: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(pegar_sessao)):
+    usuario = autenticar_usuario(dados_formulario.username, dados_formulario.password, session)
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+    else:
+        access_token = criar_token(usuario.id)
+        return {
+            "access_token": access_token,
             "token_type": "bearer",  
         }
-        
+
+
 @auth_router.get("/refresh")
-async def use_refresh_token(token):
-    # verificar o token
-    usuario = verificar_token(token)
-    access_token = criar_conta(usuario.id)
+async def use_refresh_token(usuario: Usuario = Depends(verificar_token)):
+    access_token = criar_token(usuario.id)
     return {
             "acess_token": access_token,
             "token_type": "bearer",
--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,9 @@
-from sqlalchemy.orm import sessionmaker
-from models import db
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import sessionmaker, Session
+from models import db, Usuario
+from jose import JWTError, jwt
+from main import SECRET_KEY, ALGORITHM
+
 
 def pegar_sessao():
     
@@ -10,3 +14,15 @@ def pegar_sessao():
     
     finally:
         session.close()
+        
+        
+def verificar_token(token: str, session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id_usuario = int(dic_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido! Verifique a validade do token e tente novamente.")
+    usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+    return usuario
